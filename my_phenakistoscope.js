0a79be95138333cf5a0980cc6aede57c799e2041
CharisK62 <Kangchar@myvuw.ac.nz>
2023-09-13 13:16:32 +1200

    loaded my images into the framework.

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -3,16 +3,17 @@ const SLICE_COUNT = 10;
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+  pScope.load_image("butterfly" , "png");
 }
 
 function setup_layers(pScope){
 
   new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
+  var layer1 = new PLayer(butterfly);
   layer1.mode( SWIRL(5) );
   layer1.set_boundary( 200, 1000 );
 
@@ -21,15 +22,17 @@ function setup_layers(pScope){
   layer2.set_boundary( 0, 400 );
 }
 
-function faces(x, y, animation, pScope){
+function butterfly(x, y, animation, pScope){
+  scale(0.4);
+  pScope.draw_image("butterfly",x,y);
   
   scale(animation.frame*2);
 
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
+  //ellipse(0,0,50,50); // draw head
+  //fill(30);
+  //ellipse(-10,-10,10,10); //draw eye
+  //ellipse(10,-10,10,10); // draw eye
+  //arc(0,10,20,10,0,180); // draw mouth
 
 }
 
